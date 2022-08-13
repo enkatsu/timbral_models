ffffd7d3a0b1eda29cece9f71c411d39be849cc7
Katsuya Endoh <endkty0509@gmail.com>
2022-08-13 15:07:19 +0900

    add __init__

--- timbral_models/__init__.py
+++ timbral_models/__init__.py
@@ -2,6 +2,7 @@ from .Timbral_Brightness import timbral_brightness
 from .Timbral_Depth import timbral_depth
 from .Timbral_Hardness import timbral_hardness
 from .Timbral_Roughness import timbral_roughness
+from .Timbral_Roughness import timbral_roughness_audio_samples
 from .Timbral_Warmth import timbral_warmth
 from .Timbral_Sharpness import timbral_sharpness
 from .Timbral_Booming import timbral_booming
